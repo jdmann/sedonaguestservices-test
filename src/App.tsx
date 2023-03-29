3d3e128fc495f2fd0f2f2db24d41e0efda66a099
Jonathan Mann <jdmann84@gmail.com>
2023-03-29 15:52:37 -0700

    create

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Heading } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Sedona Guest Services</Heading>
+      <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+        <div className="elfsight-app-21e6b26e-a7c5-4a19-876b-1240d25995c8"></div>
+    </div>
+  )
+}
+
+export default App
